ecb9e538e24b97d87a0d2a6d342a5393f7e09e75
gizelle-halo <gizelle@halo-soft.net>
2023-09-01 15:38:44 +0900

    Add lib of permissions

---
+++ lib/permissions.py
@@ -0,0 +1,28 @@
+from rest_framework.permissions import BasePermission, IsAdminUser
+
+
+def get_groups_name_lowercase(group_queryset):
+    return [group.lower() for group in group_queryset.values_list('name', flat=True)]
+
+
+class IsDjangoAdmin(IsAdminUser):
+    def has_permission(self, request, view):
+        return super(IsDjangoAdmin, self).has_permission(request, view)
+
+
+class IsACWAdmin(BasePermission):
+    def has_permission(self, request, view):
+        groups = get_groups_name_lowercase(request.user.groups)
+        return bool(request.user and 'admin' in groups)
+
+
+class IsACWStaff(BasePermission):
+    def has_permission(self, request, view):
+        groups = get_groups_name_lowercase(request.user.groups)
+        return bool(request.user and 'staff' in groups)
+
+
+class IsACWAffiliate(BasePermission):
+    def has_permission(self, request, view):
+        groups = get_groups_name_lowercase(request.user.groups)
+        return bool(request.user and 'affiliate' in groups)
