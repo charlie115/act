fcadd1304d95ee99f0b529ec6f3442f5b2b29d72
gizelle-halo <gizelle@halo-soft.net>
2023-09-20 18:12:56 +0900

    ACW-23 Add jwt authentication to ws

---
+++ lib/middleware.py
@@ -0,0 +1,38 @@
+from channels.auth import AuthMiddlewareStack
+from channels.db import database_sync_to_async
+from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.exceptions import InvalidToken
+
+
+jwt_auth = JWTAuthentication()
+
+
+@database_sync_to_async
+def get_user_from_jwt(token_key):
+    try:
+        validated_token = jwt_auth.get_validated_token(token_key)
+        user = jwt_auth.get_user(validated_token)
+    except InvalidToken:
+        user = AnonymousUser()
+    return user
+
+
+class TokenAuthMiddleware:
+    def __init__(self, app):
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        headers = dict(scope["headers"])
+
+        scope["user"] = AnonymousUser()
+        if b"authorization" in headers:
+            token_name, token_key = headers[b"authorization"].decode().split()
+            if token_name == "Bearer":
+                scope["user"] = await get_user_from_jwt(token_key)
+
+        return await self.app(scope, receive, send)
+
+
+def TokenAuthMiddlewareStack(inner):
+    return TokenAuthMiddleware(AuthMiddlewareStack(inner))
