34ff84b9f64b1ab3457aef38986809902027fc21
Ernestine Lariosa <ernestine@halo-soft.net>
2023-10-20 18:27:37 +0900

    ACW-27 Add bookmark feature for market code pairs

--- src/components/MarketCodeSelector.jsx
+++ src/components/MarketCodeSelector.jsx
@@ -1,11 +1,28 @@
 import React, { useEffect, useState } from 'react';
 
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import Menu from '@mui/material/Menu';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
 import Stack from '@mui/material/Stack';
 import SvgIcon from '@mui/material/SvgIcon';
 
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
+import BookmarkRemoveIcon from '@mui/icons-material/BookmarkRemove';
+import MenuIcon from '@mui/icons-material/Menu';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import PushPinIcon from '@mui/icons-material/PushPin';
 import PushPinOutlinedIcon from '@mui/icons-material/PushPinOutlined';
 import SyncAltIcon from '@mui/icons-material/SyncAlt';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleBookmarkMarketCodePair } from 'redux/reducers/home';
 
 import { useTranslation } from 'react-i18next';
 
@@ -15,14 +32,29 @@ import DropdownMenu from 'components/DropdownMenu';
 import { MARKET_CODE_LIST } from 'constants/lists';
 
 function MarketCodeSelector({ onChange }) {
+  const dispatch = useDispatch();
+
   const { i18n, t } = useTranslation();
 
   const [marketCodeList, setMarketCodeList] = useState([]);
   const [targetMarketCode, setTargetMarketCode] = useState(0);
   const [originMarketCode, setOriginMarketCode] = useState(5);
-
   const [targetAnchorEl, setTargetAnchorEl] = useState(null);
   const [originAnchorEl, setOriginAnchorEl] = useState(null);
+
+  const [menuAnchorEl, setMenuAnchorEl] = useState(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const bookmarkedMarketCodePairs = useSelector(
+    (state) => state.home.bookmarkedMarketCodePairs
+  );
+
+  const handleMenuClose = () => {
+    setMenuOpen(false);
+    setMenuAnchorEl(null);
+  };
 
   useEffect(() => {
     if (onChange && marketCodeList.length > 0)
@@ -54,16 +86,30 @@ function MarketCodeSelector({ onChange }) {
     );
   }, [i18n.language]);
 
+  useEffect(() => {
+    const selectedPair = bookmarkedMarketCodePairs.find(
+      (pair) => pair[0] === targetMarketCode && pair[1] === originMarketCode
+    );
+    setIsBookmarked(!!selectedPair);
+  }, [bookmarkedMarketCodePairs, targetMarketCode, originMarketCode]);
+
   return (
     <Stack
       direction="row"
-      spacing={2}
+      spacing={1}
       sx={{
         alignItems: 'center',
         flex: 0.9,
         mb: { xs: 3, md: 0 },
       }}
     >
+      <MoreVertIcon
+        onClick={(event) => {
+          setMenuAnchorEl(event.currentTarget);
+          setMenuOpen((state) => !state);
+        }}
+        sx={{ cursor: 'pointer', ':hover': { opacity: 0.7 } }}
+      />
       <DropdownMenu
         options={marketCodeList}
         value={marketCodeList[targetMarketCode]}
@@ -105,6 +151,80 @@ function MarketCodeSelector({ onChange }) {
         buttonStyle={{ justifyContent: 'start', px: 2 }}
         containerStyle={{ alignSelf: 'stretch', flex: 1.5 }}
       />
+      <Menu
+        id="market-code-selector-menu"
+        aria-controls={menuOpen ? 'market-code-selector-menu' : undefined}
+        aria-expanded={menuOpen ? 'true' : undefined}
+        aria-haspopup="true"
+        anchorEl={menuAnchorEl}
+        autoFocus={false}
+        open={menuOpen}
+        onClose={handleMenuClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <MenuItem disabled>{t('Currently Selected Pair')}</MenuItem>
+        <ListItem
+          secondaryAction={
+            <IconButton
+              edge="end"
+              aria-label="bookmark"
+              onClick={() =>
+                dispatch(
+                  toggleBookmarkMarketCodePair([
+                    targetMarketCode,
+                    originMarketCode,
+                  ])
+                )
+              }
+            >
+              {isBookmarked ? (
+                <BookmarkRemoveIcon color="accent" />
+              ) : (
+                <BookmarkAddIcon />
+              )}
+            </IconButton>
+          }
+          sx={{ ':hover': { backgroundColor: 'unset' } }}
+        >
+          <ListItemText>{marketCodeList[targetMarketCode]?.label}</ListItemText>
+          <SyncAltIcon color="secondary" fontSize="small" sx={{ mx: 1 }} />
+          <ListItemText>{marketCodeList[originMarketCode]?.label}</ListItemText>
+        </ListItem>
+        <Divider />
+        <MenuItem disabled>{t('Bookmarked Pairs')}</MenuItem>
+        {bookmarkedMarketCodePairs
+          .filter(
+            (pair) =>
+              !(pair[0] === targetMarketCode && pair[1] === originMarketCode)
+          )
+          .map((pair) => (
+            <ListItem
+              key={pair.join()}
+              secondaryAction={
+                <IconButton
+                  edge="end"
+                  aria-label="bookmark"
+                  onClick={() =>
+                    dispatch(
+                      toggleBookmarkMarketCodePair([
+                        targetMarketCode,
+                        originMarketCode,
+                      ])
+                    )
+                  }
+                >
+                  <BookmarkRemoveIcon color="accent" />
+                </IconButton>
+              }
+              sx={{ ':hover': { backgroundColor: 'unset' } }}
+            >
+              <ListItemText>{marketCodeList[pair[0]]?.label}</ListItemText>
+              <SyncAltIcon color="secondary" fontSize="small" sx={{ mx: 1 }} />
+              <ListItemText>{marketCodeList[pair[1]]?.label}</ListItemText>
+            </ListItem>
+          ))}
+      </Menu>
     </Stack>
   );
 }
--- src/redux/reducers/home.js
+++ src/redux/reducers/home.js
@@ -2,7 +2,11 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import drfAuthApi from 'redux/api/drf/auth';
 
-const initialState = { favoriteAssets: {}, priceView: 'kimp' };
+const initialState = {
+  bookmarkedMarketCodePairs: [],
+  favoriteAssets: {},
+  priceView: 'kimp',
+};
 
 export const homeSlice = createSlice({
   name: 'home',
@@ -15,6 +19,17 @@ export const homeSlice = createSlice({
     },
     removeLocalFavoriteAsset: (state, { payload }) => {
       state.favoriteAssets?.[payload.marketCodeKey]?.splice(payload.id, 1);
+    },
+    toggleBookmarkMarketCodePair: (state, { payload }) => {
+      const selectedPair = state.bookmarkedMarketCodePairs.find(
+        (pair) => pair[0] === payload[0] && pair[1] === payload[1]
+      );
+      if (selectedPair)
+        state.bookmarkedMarketCodePairs =
+          state.bookmarkedMarketCodePairs.filter(
+            (item) => !(item[0] === payload[0] && item[1] === payload[1])
+          );
+      else state.bookmarkedMarketCodePairs.push(payload);
     },
     togglePriceView: (state, { payload }) => {
       state.priceView = payload;
@@ -30,6 +45,7 @@ export const homeSlice = createSlice({
 export const {
   addLocalFavoriteAsset,
   removeLocalFavoriteAsset,
+  toggleBookmarkMarketCodePair,
   togglePriceView,
 } = homeSlice.actions;
 
