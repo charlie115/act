46ba6523e72a916a280facdc521bb0ff910b8538
gizelle-halo <gizelle@halo-soft.net>
2024-01-09 19:16:41 +0900

    ACW-87 Add signal to stop/delete/start pm2 process when bot username is updated

--- apps/socialaccounts/signals.py
+++ apps/socialaccounts/signals.py
@@ -1,15 +1,49 @@
 import subprocess
 
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
 
 from socialaccounts.models import ProxySocialApp
 
 
+@receiver(pre_save, sender=ProxySocialApp, dispatch_uid="signals.update_bot_service")
+def update_bot_service(sender, instance, **kwargs):
+    if instance.provider == "telegram":
+        try:
+            original = sender.objects.get(pk=instance.pk)
+        except sender.DoesNotExist:
+            pass
+        else:
+            if original.client_id != instance.client_id:
+                subprocess.Popen(
+                    [
+                        "pm2",
+                        "stop",
+                        original.client_id,
+                    ]
+                )
+                subprocess.Popen(
+                    [
+                        "pm2",
+                        "delete",
+                        original.client_id,
+                    ]
+                )
+                subprocess.Popen(
+                    [
+                        "pm2",
+                        "start",
+                        f"python manage.py telebot {instance.client_id}",
+                        "--name",
+                        instance.client_id,
+                        "--namespace",
+                        "telebot",
+                    ]
+                )
+
+
 @receiver(post_save, sender=ProxySocialApp, dispatch_uid="signals.spawn_bot_service")
 def spawn_bot_service(sender, instance, created, **kwargs):
-    print("spawn_bot_service")
-
     if created and instance.provider == "telegram":
         subprocess.Popen(
             [
@@ -24,8 +58,8 @@ def spawn_bot_service(sender, instance, created, **kwargs):
         )
 
 
-@receiver(post_delete, sender=ProxySocialApp, dispatch_uid="signals.stop_bot_service")
-def stop_bot_service(sender, instance, **kwargs):
+@receiver(post_delete, sender=ProxySocialApp, dispatch_uid="signals.remove_bot_service")
+def remove_bot_service(sender, instance, **kwargs):
     if instance.provider == "telegram":
         subprocess.Popen(
             [
