f3c9481cee5d11faad9b2cd4a8fbeeccb3466825
gizelle-halo <gizelle@halo-soft.net>
2024-02-07 19:24:27 +0900

    Only start bot services in commands that runs the server

--- apps/socialaccounts/apps.py
+++ apps/socialaccounts/apps.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.apps import AppConfig
 
 
@@ -12,12 +14,14 @@ class SocialaccountsConfig(AppConfig):
         from . import models
         from . import telegram
 
-        bots = models.ProxySocialApp.objects.filter(provider="telegram")
-        for bot in bots:
-            result = subprocess.run(
-                ["pm2", "describe", bot.client_id],
-                capture_output=True,
-                text=True,
-            )
-            if f"{bot.client_id} doesn't exist" in result.stderr:
-                telegram.start_pm2_process(bot.client_id)
+        # Bot services should also start every time the app is started
+        if "runserver" in sys.argv or "daphne" in sys.argv[0]:
+            bots = models.ProxySocialApp.objects.filter(provider="telegram")
+            for bot in bots:
+                result = subprocess.run(
+                    ["pm2", "describe", bot.client_id],
+                    capture_output=True,
+                    text=True,
+                )
+                if f"{bot.client_id} doesn't exist" in result.stderr:
+                    telegram.start_pm2_process(bot.client_id)
