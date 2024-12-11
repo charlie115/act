8fc917dc5a45ca6672244db617e424b22186581e
Charlie <ckddjs115@naver.com>
2024-12-11 15:40:32 +0900

    uncommented telegram start

--- apps/socialaccounts/apps.py
+++ apps/socialaccounts/apps.py
@@ -15,19 +15,19 @@ class SocialaccountsConfig(AppConfig):
         from . import telegram
 
         # Bot services should also start every time the app is started
-        # if "runserver" in sys.argv or "daphne" in sys.argv[0]:
-        #     bots = models.ProxySocialApp.objects.filter(provider="telegram")
-        #     for bot in bots:
-        #         processes = ["telegram_send_message", "telegram_command"]
-        #         for process in processes:
-        #             result = subprocess.run(
-        #                 ["pm2", "describe", f"{bot.client_id}.{process}"],
-        #                 capture_output=True,
-        #                 text=True,
-        #             )
-        #             if f"{bot.client_id}.{process} doesn't exist" in result.stderr:
-        #                 telegram.start_pm2_process(bot.client_id, process)
-        #             else:
-        #                 telegram.stop_pm2_process(bot.client_id, process)
-        #                 telegram.delete_pm2_process(bot.client_id, process)
-        #                 telegram.start_pm2_process(bot.client_id, process)
+        if "runserver" in sys.argv or "daphne" in sys.argv[0]:
+            bots = models.ProxySocialApp.objects.filter(provider="telegram")
+            for bot in bots:
+                processes = ["telegram_send_message", "telegram_command"]
+                for process in processes:
+                    result = subprocess.run(
+                        ["pm2", "describe", f"{bot.client_id}.{process}"],
+                        capture_output=True,
+                        text=True,
+                    )
+                    if f"{bot.client_id}.{process} doesn't exist" in result.stderr:
+                        telegram.start_pm2_process(bot.client_id, process)
+                    else:
+                        telegram.stop_pm2_process(bot.client_id, process)
+                        telegram.delete_pm2_process(bot.client_id, process)
+                        telegram.start_pm2_process(bot.client_id, process)
